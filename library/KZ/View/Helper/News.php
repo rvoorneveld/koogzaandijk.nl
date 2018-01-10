59b1eca1d47269dfd3334e4b7ee1ed3214e78b31
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-01-10 22:12:27 +0100

    Mixed blog with news items

--- library/KZ/View/Helper/News.php
+++ library/KZ/View/Helper/News.php
@@ -1,89 +1,122 @@
 <?php
+
 class KZ_View_Helper_News extends Zend_View_Helper_Abstract
 {
-	
-	public function news($booIsMobile = false)
-	{
-		$objConfig			= Zend_Registry::get('Zend_Config');
-		
-		$objRequest			= Zend_Controller_Front::getInstance()->getRequest();
-		
-		$strControllerName 	= $objRequest->getParam('controller');
-		$strActionName 		= $objRequest->getParam('action');
-		$strTitle			= $objRequest->getParam('title');
-		
-		if($strControllerName == 'page' && $strActionName == 'index') {
-		
-			// Set Models
-			$objModelNews		= new KZ_Models_News();
-			$objModelAgenda		= new KZ_Models_Agenda();
-			$objModelCategories	= new KZ_Models_Categories();
-			$objModelSettings	= new KZ_Models_Settings();
-			$objModelPage		= new KZ_Models_Pages();
 
-			// Get Title Slug
-			$strTitleSlug		= $objRequest->getParam('title');
-			
-			// Set Defaults
-			$intCategoryID		= false;
-			$arrCategory		= false;
-			
-			if($strTitleSlug != 'home') {
-				
-				// Get Category By Category Name
-				$arrCategory	= $objModelCategories->getCategoryBySlug($strTitleSlug);
-				$intCategoryID	= ((isset($arrCategory) && is_array($arrCategory) && count($arrCategory) > 0) ? $arrCategory['category_id'] : false);
+    public function news($booIsMobile = false)
+    {
+        $objConfig = Zend_Registry::get('Zend_Config');
 
-			}
-			
-			// Set Content Type ID - Carousel (1) and Top (2)
-			$intContentTypeID 	= array(1,2);
-			
-			// Set Status - active
-			$intStatus			= 1;
-			
-			// Set Limit
-			$intLimit			= (($booIsMobile === true) ? $objConfig->news->maxRelatedMobile : $objConfig->news->maxRelated);
-			
-			// Get News items
-			$arrNewsItems		= $objModelNews->getNews($intContentTypeID, $intCategoryID, $intStatus, false, $intLimit);
-			
-			// Set Current Date
-			$objDate			= new Zend_Date();
-			$strDate			= $objDate->toString('yyyy-MM-dd');
-			
-			// Get Agenda items
-			$arrAgendaItems		= $objModelAgenda->getAgenda($strDate, $intStatus, 8);
-			
-			// Get News Categories
-			$arrCategories		= $objModelCategories->getCategories('category_id');
-			
-			// Get Topstory
-			$arrTopstory		= $objModelSettings->getSettingsByKey('topstory');
-			
-			// Check if Topstory was found
-			if(isset($arrTopstory) && is_array($arrTopstory) && count($arrTopstory) > 0) {
-				
-				$arrTopstory	= $objModelPage->getPageContent($arrTopstory['value']);
-				
-			}
-			
-			// Get News Categories
-			$intStatus				= 1;
-			$arrNewsCategories 		= $objModelNews->getNewsCategories($intStatus);
-			
-			return $this->view->partial('snippets/news.phtml', array(
-				'news'				=> $arrNewsItems,
-				'agenda'			=> $arrAgendaItems,
-				'category'			=> $arrCategory,
-				'categories'		=> $arrCategories,
-				/*'topstory'		=> $arrTopstory,*/
-				'news_categories'	=> array_slice($arrNewsCategories,0,7),
-				'title'				=> $strTitle
-			));
+        $objRequest = Zend_Controller_Front::getInstance()->getRequest();
 
-		}
-		
-	}
-	
+        $strControllerName = $objRequest->getParam('controller');
+        $strTitle = $objRequest->getParam('title');
+
+        if (true === in_array($strControllerName, ['page', 'index',], true)) {
+            $objModelNews = new KZ_Models_News();
+            $objModelAgenda = new KZ_Models_Agenda();
+            $objModelCategories = new KZ_Models_Categories();
+            $objModelSettings = new KZ_Models_Settings();
+            $objModelPage = new KZ_Models_Pages();
+            $objModelBlog = new KZ_Models_Blog();
+
+            // Get Title Slug
+            $strTitleSlug = $objRequest->getParam('title');
+
+            // Set Defaults
+            $intCategoryID = false;
+            $arrCategory = false;
+
+            if ('home' !== $strTitleSlug) {
+                $arrCategory = $objModelCategories->getCategoryBySlug($strTitleSlug);
+                $intCategoryID = (false === empty($arrCategory) && true === is_array($arrCategory)) ? $arrCategory['category_id'] : false;
+
+            }
+
+            // Set Content Type ID - Carousel (1) and Top (2)
+            $intContentTypeID = [1, 2];
+
+            // Set Status - active
+            $intStatus = 1;
+
+            // Set Limit
+            $intLimit = ((true === $booIsMobile) ? $objConfig->news->maxRelatedMobile : $objConfig->news->maxRelated);
+
+            // Get News items
+            $arrNewsItems = $objModelNews->getNews($intContentTypeID, $intCategoryID, $intStatus, false, $intLimit);
+            $arrNewsItemsOrdered = $this->orderByDateAndTime($arrNewsItems);
+
+            // Combine News And Blog items
+            $arrOrderedNews = $this->orderByDateAndTime($objModelBlog->getLatestBlogItems($intLimit), 'blog', $arrNewsItemsOrdered);
+
+            // Set Current Date
+            $objDate = new Zend_Date();
+            $strDate = $objDate->toString('yyyy-MM-dd');
+
+            // Get Agenda items
+            $arrAgendaItems = $objModelAgenda->getAgenda($strDate, $intStatus, 8);
+
+            // Get News Categories
+            $arrCategories = $objModelCategories->getCategories('category_id');
+
+            // Get Topstory
+            $arrTopstory = $objModelSettings->getSettingsByKey('topstory');
+
+            // Check if Topstory was found
+            if (false === empty($arrTopstory) && true === is_array($arrTopstory)) {
+                $arrTopstory = $objModelPage->getPageContent($arrTopstory['value']);
+            }
+
+            // Get News Categories
+            $intStatus = 1;
+            $arrNewsCategories = $objModelNews->getNewsCategories($intStatus);
+
+            return $this->view->partial('snippets/news.phtml', [
+                'news' => array_slice($arrOrderedNews, 0, $intLimit),
+                'agenda' => $arrAgendaItems,
+                'category' => $arrCategory,
+                'categories' => $arrCategories,
+                'topstory' => $arrTopstory,
+                'news_categories' => array_slice($arrNewsCategories, 0, 7),
+                'title' => $strTitle
+            ]);
+
+        }
+
+    }
+
+    /**
+     * @param $arrData
+     * @param null $strType
+     * @param null $arrReturn
+     * @return array|null
+     * @throws Zend_Date_Exception
+     */
+    private function orderByDateAndTime($arrData, $strType = null, $arrReturn = null)
+    {
+        if (null === $arrReturn) {
+            $arrReturn = [];
+        }
+
+        if (false === empty($arrData) && true === is_array($arrData)) {
+            $objDate = new Zend_Date();
+            foreach ($arrData as $arrDataRow) {
+                switch ($strType) {
+                    case 'blog':
+                        $objDate->set($arrDataRow['created'], 'yyyy-MM-dd HH:mm:ss');
+                        break;
+                    case 'news':
+                    default:
+                        $objDate->setDate($arrDataRow['date'], 'yyyy-MM-dd');
+                        $objDate->setTime($arrDataRow['time'], 'HH');
+                        break;
+                }
+                $intTimeStamp = (int)$objDate->toValue();
+                $arrReturn[$intTimeStamp] = $arrDataRow;
+            }
+        }
+        krsort($arrReturn);
+        return $arrReturn;
+    }
+
 }
