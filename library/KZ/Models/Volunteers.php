c2cd8556ff7293bce41da2e044b40a2fcef1eb8d
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-03-05 15:26:35 +0100

    added function for getting volunteers by date

--- library/KZ/Models/Volunteers.php
+++ library/KZ/Models/Volunteers.php
@@ -3,4 +3,15 @@ class KZ_Models_Volunteers extends KZ_Controller_Table
 {
     protected $_name = 'volunteer';
     protected $_primary = 'id';
+
+    public function getVolunteersByDate($strDateStart, $strDateEnd)
+    {
+        $objQuery = $this->select()
+                    ->where('date >= ?', $strDateStart)
+                    ->where('date <= ?', $strDateEnd)
+                    ->order('date')
+                    ->order('timeStart');
+        return $this->returnData($objQuery);
+    }
+
 }
