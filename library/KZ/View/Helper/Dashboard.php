a43a612a8b48ea959e610f9656a5c16873d8e587
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-03-09 09:38:12 +0100

    Fixes for GA in Dashboard

--- library/KZ/View/Helper/Dashboard.php
+++ library/KZ/View/Helper/Dashboard.php
@@ -3,13 +3,11 @@
 class KZ_View_Helper_Dashboard extends Zend_View_Helper_Abstract
 {
 
-    public function getGaCharts()
-    {
-
+    public function getGaCharts() {
         $objConfig = Zend_Registry::get('Zend_Config');
         $strUaCode = $objConfig->default->application->analytics;
 
-        if (!class_exists('Google_Client')) {
+        if (false === class_exists('Google_Client')) {
             echo 'Install the Google Client Library';
             exit;
         }
@@ -20,37 +18,34 @@ class KZ_View_Helper_Dashboard extends Zend_View_Helper_Abstract
         // Get the Google Service Account Config File
         $strConfigFile = APPLICATION_PATH.DIRECTORY_SEPARATOR.'configs'.DIRECTORY_SEPARATOR.'google--service-account.json';
 
-        $objClient->setAuthConfig(json_decode(file_get_contents($strConfigFile),true));
+        $objClient->setAuthConfig(json_decode(file_get_contents($strConfigFile), true));
         $objClient->setScopes([Google_Service_Analytics::ANALYTICS_READONLY,]);
 
-        if ($objClient->isAccessTokenExpired()) {
+        $objClient->useApplicationDefaultCredentials();
+
+        if (true === $objClient->isAccessTokenExpired()) {
             $objClient->refreshTokenWithAssertion();
         }
 
-        if($arrAccessToken = $objClient->getAccessToken() !== null) {
-            try {
+        if (null !== ($arrAccessToken = $objClient->getAccessToken())) {
+            //try {
                 $objManagementProfiles = (new Google_Service_Analytics($objClient))->management_profiles;
-                $intDataId = current($objManagementProfiles->listManagementProfiles((int)explode('-',$strUaCode)[1],$strUaCode)->getItems())->getId();
-                return $this->googleanalytics($arrAccessToken['access_token'],$intDataId);
-            } catch (Exception $objException) {
-                var_dump($objException);
-                exit;
-            }
+                $intDataId = current($objManagementProfiles->listManagementProfiles((int)explode('-', $strUaCode)[1], $strUaCode)->getItems())->getId();
+                return $this->googleanalytics($arrAccessToken['access_token'], $intDataId);
+//            } catch (Exception $objException) {
+//                //var_dump($objException);
+//                die('Could not connect to Analytics API');
+//                exit;
+//            }
         }
 
     }
 
-    public function googleanalytics($strAuthToken,$intDataId)
+    public function googleanalytics($strAuthToken, $intDataId)
     {
-        // The template file to render
-        $strTemplateFile = 'googleanalytics.phtml';
-
-        // Create new View
-        $this->objView = (new Zend_View())->setScriptPath(APPLICATION_PATH.'/modules/admin/views/scripts/dashboard/')
-            ->setHelperPath(LIBRARY_PATH.'/Websitebuilder/View/Helper/');
-
-        // Parse variables to the view
-        return $this->renderPartial($strTemplateFile,[
+        $objView = new Zend_View();
+        $objView->setScriptPath(APPLICATION_PATH.'/modules/admin/views/scripts/dashboard/');
+        return $objView->partial('index.phtml', [
             'strAuthToken' => $strAuthToken,
             'intDataId' => $intDataId,
         ]);
