fb282dbb32412303cd5c0ee19dcd13b63955f554
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-08-27 20:15:13 +0200

    Updated start date for new season

--- library/KZ/View/Helper/Date.php
+++ library/KZ/View/Helper/Date.php
@@ -68,7 +68,7 @@ class KZ_View_Helper_Date extends Zend_View_Helper_Abstract
 	
 	public function getSeasonStartDate()
 	{
-		return '2017-08-15';
+		return '2018-08-15';
 	}
 
     public function date()
