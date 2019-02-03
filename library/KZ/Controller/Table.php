19d3b7cb96b242a28ccd040f75a2e51d15574d82
Rick Voorneveld <r.voorneveld@hoteliers.com>
2019-02-03 11:19:03 +0100

    PHP 5.6 -> 7.2 upgrade

--- library/KZ/Controller/Table.php
+++ library/KZ/Controller/Table.php
@@ -10,46 +10,20 @@ class KZ_Controller_Table extends Zend_Db_Table_Abstract
     }
 
     public function returnData($strQuery, $strReturnType = 'array', $strFetchType = 'fetchAll')
-	{
-		
-		// Check if Query string is not empty
-		if(! empty($strQuery)) {
-			
-			// Set Default Return type
-			$mixReturnData = (($strReturnType == 'array') ? array() : new stdClass());
-			
-			// Check Fetch Type
-			if($strFetchType == 'fetchAll') {
-				
-				// Fetch Multiple Rows
-				$objResult = $this->fetchAll($strQuery);
-			
-			} else {
-				// Fetch Single Row
-				$objResult = $this->fetchRow($strQuery);
-			}
-			
-			// Check if Result is not null and > 0
-			if(! is_null($objResult) && count($objResult) > 0) {
-				
-				// Check if Return Type is array
-				if($strReturnType == 'array') {
-					// Return Array with Data
-					return $objResult->toArray();
-				}
-				// Return Object with Data
-				return $objResult;
-				
-			}
-			// Return Default Object or Array
-			return $mixReturnData;
-			
-		}
+    {
+        if (false === empty($strQuery)) {
+            $mixReturnData = 'array' === $strReturnType ? [] : new stdClass();
+            $method = ('fetchAll' === $strFetchType ? $strFetchType : 'fetchRow');
 
-		// Return false
-		return false;
-		
-	}
+            if (null !== ($objResult = $this->$method($strQuery))
+                && count($returnArray = $objResult->toArray()) > 0
+            ) {
+                return 'array' === $strReturnType ? $returnArray : $objResult;
+            }
+            return $mixReturnData;
+        }
+        return false;
+    }
 	
 	public function _truncate($strTableName)
 	{
